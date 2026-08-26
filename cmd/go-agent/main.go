16a57aeaf2c78bd1d777eb317852f55197c14bd8
Johann <methamorphe@gmail.com>
2026-08-26 21:42:01 +0200

    chore: add development foundations

---
+++ cmd/go-agent/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Methamorphe/go-agent/internal/app"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	defer stop()
+
+	logger := slog.New(
+		slog.NewTextHandler(os.Stderr, nil),
+	)
+
+	application := app.New(logger)
+
+	if err := application.Run(ctx); err != nil {
+		logger.Error(
+			"application stopper with an error",
+			"error", err,
+		)
+
+		return 1
+	}
+
+	return 0
+}
