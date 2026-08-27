576704f81a8e3405fa0ca02878d99b201d12c957
Johann <40505181+Methamorphe@users.noreply.github.com>
2026-08-27 14:53:30 +0200

    fix(control): dial Windows named pipe client

--- internal/control/dial_windows.go
+++ internal/control/dial_windows.go
@@ -1,21 +1,20 @@
-//go:build !windows
+//go:build windows
 
 package control
 
 import (
 	"context"
 	"net"
+
+	winio "github.com/Microsoft/go-winio"
 )
 
 func dial(
 	ctx context.Context,
 	address string,
 ) (net.Conn, error) {
-	var dialer net.Dialer
-
-	return dialer.DialContext(
+	return winio.DialPipeContext(
 		ctx,
-		"unix",
 		address,
 	)
 }
